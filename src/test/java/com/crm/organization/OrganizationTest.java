5ccf3ee0749eb00c86890923cb1e4d08403f8445
rekha <DELL@DESKTOP-L72NU8B>
2022-05-13 12:37:45 +0530

    first commit sample

---
+++ src/test/java/com/crm/organization/OrganizationTest.java
@@ -0,0 +1,30 @@
+package com.crm.organization;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	//hello bindhu
+	@Test(groups = {"smokeTest"})
+	public void createOrg()
+	{
+		System.out.println("create org");
+	}
+	//hell
+	
+	@Test(groups = {"regressionTest"})
+	public void gihub()
+	{
+		//changes 1 rekhabindu
+		System.out.println("delete org");
+	}
+	@Test(groups = {"regressionTest"})
+	public void modifyOrg()
+	{
+		System.out.println("modify org");
+	}
+	@Test(groups = {"regressionTest"})
+	public void deleteOrg()
+	{
+		System.out.println("delete org");
+	}
+}
